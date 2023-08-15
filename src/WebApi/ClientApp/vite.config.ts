5331ba630ffc2cd1ae13c42225e7314880aeb821
JershDervis <joshdavis16@outlook.com>
2023-08-16 09:03:13 +1000

    Initialise Sveltekit app

---
+++ src/WebApi/ClientApp/vite.config.ts
@@ -0,0 +1,24 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+    server: {
+        port: 5173,
+        https: true,
+        proxy: {
+            '/api': {
+                target: 'https://localhost:7255',
+                secure: false
+            },
+            // '/ws': {
+            //     target: 'ws://localhost:5024',
+            //     ws: true
+            // },
+        },
+        host: true
+    }
+});
